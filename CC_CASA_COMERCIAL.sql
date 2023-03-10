56007f2ac83c6788266512078804d84ce7e45106
Mr. Ed <edgardo.maulen@inacapmail.cl>
2023-03-09 21:49:17 -0300

    Eliminar archivos SQL
    Se habia subido la base de datos

--- CC_CASA_COMERCIAL.sql
+++
@@ -1,5 +0,0 @@
-insert into CC_CASA_COMERCIAL (Id, Nombre, Descripcion) values (1, 'Mair', ',./;''[]\-=');
-insert into CC_CASA_COMERCIAL (Id, Nombre, Descripcion) values (2, 'Adi', 'test⁠test‫');
-insert into CC_CASA_COMERCIAL (Id, Nombre, Descripcion) values (3, 'Patty', '-1E+02');
-insert into CC_CASA_COMERCIAL (Id, Nombre, Descripcion) values (4, 'Rab', '울란바토르');
-insert into CC_CASA_COMERCIAL (Id, Nombre, Descripcion) values (5, 'Cate', 'בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץ');
